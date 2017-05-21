de3f481bbbdfeb714fa0602993c1284ca0273736
Muriel Nahmani <muriel.nahmani@gmail.com>
2017-05-22 00:37:14 +0200

    Add new class Address and its link to Room

--- cercleforme/admin.py
+++ cercleforme/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import CourseType, Course, Room
+from .models import CourseType, Course, Room, Address
 
 # Register your models here.
 admin.site.register(CourseType)
 admin.site.register(Course)
 admin.site.register(Room)
+admin.site.register(Address)
--- cercleforme/models.py
+++ cercleforme/models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-import datetime
 from django.db import models
 
 DAY_CHOICES = (
@@ -23,8 +22,18 @@ class CourseType(models.Model):
         return self.name
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=150)
+    zipcode = models.IntegerField()
+    city = models.CharField(max_length=150, default="Paris")
+
+    def __str__(self):
+        return self.street
+
+
 class Room(models.Model):
     name = models.CharField(max_length=150)
+    address = models.ForeignKey(Address)
 
     def __str__(self):
         return self.name
