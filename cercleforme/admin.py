2e0975ac50d0f4f511e93891cda9c349be433b73
Muriel Nahmani <muriel@apple.com>
2017-05-21 23:17:20 +0200

    Add models with v1 fields and migrations

--- cercleforme/admin.py
+++ cercleforme/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import CourseType, Course, Room
+
 # Register your models here.
+admin.site.register(CourseType)
+admin.site.register(Course)
+admin.site.register(Room)
--- cercleforme/models.py
+++ cercleforme/models.py
@@ -1,11 +1,45 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
 
 
+DAY_CHOICES = (
+    ('MO', 'Monday'),
+    ('TU', 'Tuesday'),
+    ('WE', 'Wednesday'),
+    ('TH', 'Thursday'),
+    ('FR', 'Friday'),
+    ('SA', 'Saturday'),
+    ('SU', 'Sunday'),
+)
+
+
 # Create your models here.
-class CourseTypes(models.Model):
+class CourseType(models.Model):
     name = models.CharField(max_length=150)
     color = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150)
+    type = models.ForeignKey(CourseType)
+    room = models.ForeignKey(Room)
+    day = models.CharField(max_length=150, choices=DAY_CHOICES)
+    hour = models.TimeField()
+    duration = models.DurationField(default=datetime.timedelta(hours=1))
+
+    def __str__(self):
+        return self.name
 
 
