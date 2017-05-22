037c7f5589b3d2d159b582b1fb41dacb0bc9c9c8
Lucas Guibert <lucas.guibert@viacesi.fr>
2017-05-22 21:13:57 +0200

    Add fucking 127 to allowed host

--- sportapp/settings.py
+++ sportapp/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,10 +25,10 @@ SECRET_KEY = 'h*zl)!hl1%6d*=b2%pl#-(^v_!v6!gfek4hw^bx^2apn6o9jje'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-	"localhost",
-	"circleapp.lucasguibert.com"
+    "localhost",
+    "127.0.0.1",
+    "circleapp.lucasguibert.com"
 ]
-
 
 # Application definition
 
@@ -74,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sportapp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -84,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -104,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -117,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
