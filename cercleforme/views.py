c41fc84c9066d333a4a6c68f7100b5198b3eb3a2
Lucas Guibert <lucas.guibert@viacesi.fr>
2017-05-22 19:13:09 +0200

    Add possibility to select two of something

--- cercleforme/views.py
+++ cercleforme/views.py
@@ -3,7 +3,6 @@ from django.views.decorators.http import require_POST
 from django.http.response import JsonResponse, HttpResponse
 from django.core import serializers
 
-
 from cercleforme.models import CourseType, Room, Course
 
 
@@ -16,14 +15,35 @@ def home(request):
 
 @require_POST
 def search_course(request):
-    type_id = request.POST.get("type", None)
-    room_id = request.POST.get("room", None)
-    time = request.POST.get("time", None)
+    day_id = request.POST.getlist("day[]", request.POST.get("day", None))
+    type_id = request.POST.getlist("type[]", request.POST.get("type", None))
+    room_id = request.POST.getlist("room[]", request.POST.get("room", None))
+    time = request.POST.getlist("time[]", request.POST.get("time", None))
 
-    if type_id is None or room_id is None or time is None:
-        return JsonResponse(status=500)
+    courses = Course.objects.all()
 
-    type = CourseType.objects.get(id=type_id)
-    room = Room.objects.get(id=room_id)
-    courses = [{"id": course.id, "name": course.name, "start_time": course.start_time, "end_time": course.end_time} for course in Course.objects.filter(start_time__gte=time, room=room, type=type)]
-    return JsonResponse({"courses":courses})
+    if type_id:
+        courses = courses.filter(type_id__in=CourseType.objects.filter(id__in=type_id).values_list("id", flat=True))
+
+    if room_id:
+        courses = courses.filter(room_id__in=Room.objects.filter(id__in=room_id).values_list("id", flat=True))
+
+    if day_id:
+        # TODO: Mumu ?
+        pass
+
+    if time:
+        # TODO: Mumu ?
+        pass
+
+    return JsonResponse(
+        {"courses": [
+            {
+                "id": course.id,
+                "name": course.name,
+                "start_time": course.start_time,
+                "end_time": course.end_time
+            }
+            for course in courses]
+        }
+    )
